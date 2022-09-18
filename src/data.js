71b68a7aab834dc57a254d202dd308a453f375f5
Stefan <84741877+StefanLambic@users.noreply.github.com>
2022-09-18 18:52:08 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const text = [
+  `Neka bude borba neprestana,
+  neka bude što biti ne može.
+  Na groblju će iznići cvijeće
+  za daleko meko pokoljenje.`,
+
+  `Zdravo tvoja glava na ramena,
+  ti ćeš pušku drugu nabaviti
+  a u ruke Mandušića Vuka
+  biće svaka puška ubojita.`,
+
+  `Pokoljenje za pjesnu stvoreno
+  vile će se grabit o vjekove
+  da vam vjence dostojne sapletu
+  vaš će primjer učiti pjevača
+  kako treba s besmrtnošću zborit.`,
+
+  `Vuk na ovcu svoje pravo ima
+  ka tirjanin na slaba čoeka;
+  al tirjanstvu stati nogom za vrat;
+  dovesti ga k poznaniju prava,
+  to je ljudska dužnost najsvetija! `,
+
+  `Ćud je ženska smiješna rabota!
+  Ne zna žena ko je kakve vjere;
+  stotinu će promijenit vjerah
+  da učini što joj srce žudi.`,
+
+  `Lipo, ljepo, lepo i lijepo, bilo, bjelo,
+  belo i bijelo-listići su jednoga cvijeta,
+  u pupolj se jedan odnjihali.`,
+
+  `Strah životu kalja obraz često. Ko na brdo,
+  ak i malo,
+  stoji više vidi no onaj pod brdom. `,
+
+  `Tijelo stenje pod silom duševnom, koleba se duša u tijelu;
+  more stenje pod silom nebesnom; koleblju se u moru nebesa.
+  Niko srećan, a niko dovoljan;
+  niko miran, a niko spokojan.
+  Sve se čovjek bruka sa čovjekom;
+  gleda majmun sebe u zrcalo.`,
+];
+export default text;
